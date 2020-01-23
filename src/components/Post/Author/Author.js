0cb460c8a5f047269d88574ff971942752e61c13
HyunSangHan <gustkd3@gmail.com>
2020-01-23 09:02:34 +0900

    Edit footer message of detail pages

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -10,14 +10,14 @@ const Author = () => {
   return (
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
-        {author.bio}
+      잘못된 내용이 있다면 Pull Request로 고쳐주시거나 혹은 Issue를 통해 제게 꼭 알려주세요! 댓글도 좋습니다.
         <a
           className={styles['author__bio-twitter']}
-          href={getContactHref('github', author.contacts.github)}
+          href="https://github.com/HyunSangHan/TIL"
           rel="noopener noreferrer"
           target="_blank"
         >
-          <strong>{author.name}</strong> on Github
+          <strong>HyunSangHan > TIL</strong> on Github
         </a>
       </p>
     </div>
